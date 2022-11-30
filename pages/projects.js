d576915b062a16b981c92bd1419aad697d90d4e5
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-01 03:53:15 +0900

    Project 화면 제작

--- pages/projects.js
+++ pages/projects.js
@@ -1,22 +1,30 @@
 import Layout from "../components/layout";
 import Head from "next/head";
 import { TOKEN, DATABASE_ID } from "../config";
+import ProjectItem from "../components/projects/project-item";
 
 export default function Projects({ projects }) {
   console.log(projects);
 
   return (
     <Layout>
-      <Head>
-        <title>정상태 - 포트폴리오</title>
-        <meta name="description" content="노력하는 개발자" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>TOTAL PROJECT : {projects.results.length}</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen mb-10 px-6">
+        <Head>
+          <title>정상태 - 포트폴리오</title>
+          <meta name="description" content="노력하는 개발자" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          TOTAL PROJECT :
+          <span className="pl-4 text-blue-500">{projects.results.length}</span>
+        </h1>
 
-      {projects.results.map((aProject) => (
-        <h1>{aProject.properties.이름.title[0].plain_text}</h1>
-      ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8">
+          {projects.results.map((aProject) => (
+            <ProjectItem key={aProject.id} data={aProject} />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
