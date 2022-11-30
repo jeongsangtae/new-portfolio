9d7e1063950f8d8fe887779f99ea7f9d6d165ae5
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-01 01:30:02 +0900

    API, Next.js 데이터 가져오기

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,10 @@
 import Layout from "../components/layout";
 import Head from "next/head";
+import { TOKEN, DATABASE_ID } from "../config";
 
-export default function Projects() {
+export default function Projects({ projects }) {
+  console.log(projects);
+
   return (
     <Layout>
       <Head>
@@ -9,7 +12,50 @@ export default function Projects() {
         <meta name="description" content="노력하는 개발자" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>PROJECT</h1>
+      <h1>TOTAL PROJECT : {projects.results.length}</h1>
+
+      {projects.results.map((aProject) => (
+        <h1>{aProject.properties.이름.title[0].plain_text}</h1>
+      ))}
     </Layout>
   );
+}
+
+//빌드 타임에 호출
+export async function getStaticProps() {
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      sorts: [
+        {
+          property: "이름",
+          direction: "ascending",
+        },
+      ],
+      page_size: 100,
+    }),
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+
+  const projects = await res.json();
+
+  const projectNames = projects.results.map(
+    (aProject) => aProject.properties.이름.title[0].plain_text
+  );
+
+  console.log(`projectNames : ${projectNames}`);
+
+  return {
+    props: { projects }, // will be passed to the page component as props
+  };
 }
