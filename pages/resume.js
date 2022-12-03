db6241358ec7de076bb86c2dd6ce861f56a69c42
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-03 23:10:21 +0900

    RESUME 제작

--- pages/resume.js
+++ pages/resume.js
@@ -4,17 +4,17 @@ import Head from "next/head";
 export default function Resume() {
   return (
     <Layout>
-      <div className="lg:w-full flex flex-col">
+      <div className="flex flex-col items-center justify-center p-6 m-3">
         <Head>
           <title>정상태 - 포트폴리오</title>
           <meta name="description" content="노력하는 개발자" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <a>ABOUT ME</a>
-        <br className="border-t-2 border-white" />
-        <a>CERTIFICATE</a>
-        <br className="border-t-2 border-white" />
-        <a className="">SKILL</a>
+        <p>ABOUT ME</p>
+        <br className="border-t-2 border-slate-400" />
+        <p>CERTIFICATE</p>
+        <br className="border-t-2 border-slate-400" />
+        <p className="">SKILL</p>
       </div>
     </Layout>
   );
