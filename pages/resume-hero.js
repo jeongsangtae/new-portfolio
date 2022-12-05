985f9c5e45c3cc2595e2fcfd05d92bc2c26657d8
jeongsangtae <wjdtkdxo55@naver.com>
2022-12-05 21:08:14 +0900

    resume-hero base

--- pages/resume-hero.js
+++ pages/resume-hero.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import 증명사진 from "/public/증명사진.jpg";
+import 증명사진 from "/public/images/증명사진.jpg";
 
 export default function ResumeHero() {
   return (
@@ -21,14 +21,14 @@ export default function ResumeHero() {
                 />
               </div>
             </div>
-            <div className="lg:w-1/2 w-full leading-relaxed text-white font-bold">
+            <div className="lg:w-1/2 w-full leading-relaxed text-black dark:text-white font-bold">
               ABOUT ME
               <br />
               <p className="lg:w-1/2 w-full leading-relaxed text-gray-500 font-medium mt-5">
                 프론트엔드 개발자를 꿈꾸는 정상태입니다.
               </p>
               <br />
-              <div className="lg:w-1/2 w-full leading-relaxed text-white font-bold mt-5">
+              <div className="lg:w-1/2 w-full leading-relaxed text-black dark:text-white font-bold mt-5">
                 CERTIFICATE
               </div>
               <p className="lg:w-1/2 w-full leading-relaxed text-gray-500 font-medium mt-5">
@@ -36,28 +36,115 @@ export default function ResumeHero() {
               </p>
             </div>
           </div>
-          <div className="h-px w-full bg-slate-600 mb-10"></div>
 
-          <div class="text-gray-400 bg-slate-800 body-font">
-            <div class="container px-5 py-24 mx-auto">
-              <div class="flex flex-col text-center w-full mb-20">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+          <div className="h-px w-full bg-gray-600/20 dark:bg-slate-600/50 mb-10"></div>
+
+          <div className="text-gray-400 bg-white dark:bg-slate-800 body-font">
+            <div className="container px-5 py-24 mx-auto">
+              <div className="flex flex-col text-center w-full mb-20">
+                <h1
+                  className="sm:text-3xl text-2xl font-medium title-font mb-4
+                text-black dark:text-white"
+                >
                   SKILL
                 </h1>
-                <p class="lg:w-2/3 mx-auto leading-relaxed text-base mt-10 font-bold">
+                <p className="lg:w-2/3 mx-auto leading-relaxed text-2xl mt-10 font-bold">
                   FRONT-END
                 </p>
-                <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                  <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                    <img
-                      alt="team"
-                      class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                      src="https://dummyimage.com/100x90"
-                    />
-                    <div class="flex-grow">
-                      <h2 class="text-gray-900 title-font font-medium">
-                        Atticus Finch
-                      </h2>
+
+                <div className="flex flex-wrap -m-2">
+                  <div className="p-2 lg:w-1/3 md:w-1/2 w-full mt-10">
+                    <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        className="w-16 h-16 object-cover object-center flex-shrink-0  mr-4"
+                        src="/images/html.png"
+                      />
+                      <div className="flex-grow">
+                        <h2 className="text-gray-900 title-font font-medium">
+                          Atticus Finch
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="p-2 lg:w-1/3 md:w-1/2 w-full mt-10">
+                    <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        className="w-16 h-16 object-cover object-center flex-shrink-0 mr-4"
+                        src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-css-3-512.png"
+                      />
+                      <div className="flex-grow">
+                        <h2 className="text-gray-900 title-font font-medium">
+                          Atticus Finch
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="p-2 lg:w-1/3 md:w-1/2 w-full mt-10">
+                    <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://dummyimage.com/100x90"
+                      />
+                      <div className="flex-grow">
+                        <h2 className="text-gray-900 title-font font-medium">
+                          Atticus Finch
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <p className="lg:w-2/3 mx-auto leading-relaxed text-2xl mt-10 font-bold">
+                  BACK-END
+                </p>
+
+                <div className="flex flex-wrap -m-2">
+                  <div className="p-2 lg:w-1/3 md:w-1/2 w-full mt-10">
+                    <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://dummyimage.com/100x90"
+                      />
+                      <div className="flex-grow">
+                        <h2 className="text-gray-900 title-font font-medium">
+                          Atticus Finch
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="p-2 lg:w-1/3 md:w-1/2 w-full mt-10">
+                    <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://dummyimage.com/100x90"
+                      />
+                      <div className="flex-grow">
+                        <h2 className="text-gray-900 title-font font-medium">
+                          Atticus Finch
+                        </h2>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="p-2 lg:w-1/3 md:w-1/2 w-full mt-10">
+                    <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img
+                        alt="team"
+                        className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://dummyimage.com/100x90"
+                      />
+                      <div className="flex-grow">
+                        <h2 className="text-gray-900 title-font font-medium">
+                          Atticus Finch
+                        </h2>
+                      </div>
                     </div>
                   </div>
                 </div>
